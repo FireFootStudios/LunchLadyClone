9becdb1948910ebe8eacad8a08e28dfa36f33ffe
Casper Suetens <75252957+MrLaserbeam@users.noreply.github.com>
2025-02-24 12:11:28 +0100

    Added jackbox

---
+++ Assets/#Scripts/Casper Scripts/JackBoxTrap.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class JackBoxTrap : MonoBehaviour
+{
+    [SerializeField] private GameObject _scaryObject;
+    [SerializeField] private float _removeBoxDuration;
+    [SerializeField] private MovementModifier _movementModifier;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.TryGetComponent<PlayerN>(out PlayerN player))
+        {
+            if(_scaryObject != null)
+            {
+                _scaryObject.SetActive(true);
+                StartCoroutine(StunPlayer(player));
+            }
+        }
+    }
+
+    IEnumerator StunPlayer(PlayerN player)
+    {
+        _scaryObject.transform.LookAt(player.transform.position);
+        player.Movement.AddOrUpdateModifier(_movementModifier);
+
+        yield return new WaitForSeconds(_removeBoxDuration);
+
+        this.gameObject.SetActive(false);
+    }
+}
