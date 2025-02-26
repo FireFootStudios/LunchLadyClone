8c57955512db8200b89ad76fe3bdf0d11c7861e1
Casper Suetens <75252957+MrLaserbeam@users.noreply.github.com>
2025-02-26 15:18:33 +0100

    Video disable after finished

---
+++ Assets/#Scripts/Casper Scripts/DisableAfterVideo.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class DisableAfterVideo : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer _videoPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_videoPlayer != null && _videoPlayer.isPlaying)
+        {
+            _videoPlayer.loopPointReached += OnLoopPointReached;
+        }
+       
+    }
+
+    private void OnLoopPointReached(VideoPlayer vp)
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+}
