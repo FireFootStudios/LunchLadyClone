80929c42da5dfcde5b90804fb032569b71d99ba0
FrankyDB3 <131538717+FrankyDB3@users.noreply.github.com>
2025-02-28 15:46:49 +0100

    Adding Vivox stuff and first test

---
+++ Assets/#Scripts/SenneScripts/JoinVoiceChannel.cs
@@ -0,0 +1,46 @@
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Vivox;
+using UnityEngine;
+
+public class JoinVoiceChannel : MonoBehaviour
+{
+
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        await VivoxService.Instance.InitializeAsync();
+        await VivoxService.Instance.LoginAsync();
+        await VivoxService.Instance.JoinGroupChannelAsync("Lobby", ChatCapability.AudioOnly); 
+    }
+    async void InitializeAsync()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        await VivoxService.Instance.InitializeAsync();
+    }
+
+
+    public async void LoginToVivoxAsync()
+    {
+        LoginOptions options = new LoginOptions();
+        await VivoxService.Instance.LoginAsync(options);
+    }
+
+    public async void JoinLobbyVoice()
+    {
+        ChannelOptions channelOptions = new ChannelOptions();
+        await VivoxService.Instance.JoinGroupChannelAsync("Lobby", ChatCapability.AudioOnly); 
+        //await VivoxService.Instance.JoinEchoChannelAsync("ChannelName", ChatCapability.AudioOnly);
+    }
+
+    public async void LeaveLobbyVoice()
+    {
+        string channelToLeave = "Lobby";
+        await VivoxService.Instance.LeaveChannelAsync(channelToLeave);
+    }
+
+
+}
