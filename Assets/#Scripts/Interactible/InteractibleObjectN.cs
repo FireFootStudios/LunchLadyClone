2150fe45f900a67086ead11d8e30fb8b2b4da109
JochenRobbrecht <jochen.robbrecht@student.howest.be>
2025-03-03 16:05:33 +0100

    fix interactables anims

--- Assets/#Scripts/Interactible/InteractibleObjectN.cs
+++ Assets/#Scripts/Interactible/InteractibleObjectN.cs
@@ -9,44 +9,42 @@ public class InteractibleObjectN : NetworkBehaviour
     [SerializeField] private string _firstAnimTrigger = null;
     [SerializeField] private string _secondAnimTrigger = null;
      
-    private NetworkVariable<bool> _animPlayed = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<bool> _interacted = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
 
-        // Init...
-        _animPlayed.OnValueChanged += OnAnimPlayedChanged;
+        _interacted.OnValueChanged += OnAnimPlayedChanged;
     }
 
     private void OnAnimPlayedChanged(bool previousValue, bool newValue)
     {
+        foreach (Animator animator in _animators)
+        {
+            if (newValue) animator.SetTrigger(_firstAnimTrigger);
+            else animator.SetTrigger(_secondAnimTrigger);
+        }
     }
 
     public void PlayAnimOnInteract()
     {
         if (IsSpawned) return;
 
-        foreach (Animator animator in _animators)
+        if (!string.IsNullOrEmpty(_firstAnimTrigger) && !_interacted.Value)
         {
-            if (_firstAnimTrigger != null && !_animPlayed.Value)
-            {
-                animator.SetTrigger(_firstAnimTrigger);
-                if (_secondAnimTrigger != null)
-                    _animPlayed.Value = true;
-            }
-            else if (_secondAnimTrigger != null && _animPlayed.Value)
-            {
-                animator.SetTrigger(_secondAnimTrigger);
-                _animPlayed.Value = false;
-            }
+            _interacted.Value = true;
+        }
+        else if (!string.IsNullOrEmpty(_secondAnimTrigger) && _interacted.Value)
+        {
+            _interacted.Value = false;
         }
     }
 
     public bool CanInteract()
     {
         if (!IsSpawned || _firstAnimTrigger == null) return false;
-        if (_animPlayed.Value && string.IsNullOrEmpty(_secondAnimTrigger)) return false;
+        if (_interacted.Value && string.IsNullOrEmpty(_secondAnimTrigger)) return false;
         return true;
     }
 }
