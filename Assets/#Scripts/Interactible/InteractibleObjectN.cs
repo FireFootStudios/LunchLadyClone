b0ddff06818a1cbca01a45a03a16b466c79f4b0c
Casper Suetens <75252957+MrLaserbeam@users.noreply.github.com>
2025-03-03 10:27:32 +0100

    Coffin & Lantern changes
    I added a spot light to lantern so you can look forwards better
    I added coffins that can be openend and can be hid in from the monster

--- Assets/#Scripts/Interactible/InteractibleObjectN.cs
+++ Assets/#Scripts/Interactible/InteractibleObjectN.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 public class InteractibleObjectN : MonoBehaviour
 {
     [SerializeField] private List<Animator> _animators = new List<Animator>();
-    [SerializeField] private string _animTrigger = null;
-    [SerializeField] private GameObject _itemInsideChest;
+    [SerializeField] private string _firstAnimTrigger = null;
+    [SerializeField] private string _secondAnimTrigger = null;
+
+    private bool _isPlayingAnim = false;
 
     public void PlayAnimOnInteract()
     {
         foreach (Animator animator in _animators)
         { 
-            if(_animTrigger != null)
+            if(_firstAnimTrigger != null && !_isPlayingAnim)
             {
-                animator.SetTrigger(_animTrigger);
-                ShowItemWhenOpened();
+                animator.SetTrigger(_firstAnimTrigger);
+                if (_secondAnimTrigger !=null)
+                    _isPlayingAnim = true;
             } 
+            else if (_secondAnimTrigger != null && _isPlayingAnim)
+            {
+                animator.SetTrigger(_secondAnimTrigger);
+                _isPlayingAnim = false;
+            }
         }
-    }
-
-    private void ShowItemWhenOpened()
-    {
-        if (_itemInsideChest != null)
-            _itemInsideChest.SetActive(true);
     }
 }
